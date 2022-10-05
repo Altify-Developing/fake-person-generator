58681a485c4f324c36a26f8f6df503c7740b1fa6
Altify <Altify@mail.com>
2022-10-05 12:44:17 -0700

    Update states.js

--- js/states.js
+++ js/states.js
@@ -11,6 +11,7 @@ function readfileautomatically() {
                 document.getElementById("wintotal").innerHTML = ("State: "+combo);
             }
         }
+        client.send();
         client.open('GET', '/resc/names.txt');
         client.onreadystatechange = function()
         {

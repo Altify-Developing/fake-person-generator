22bba83f784b0e9384fa9f3ee6738b763c4e0f21
Altify <Altify@mail.com>
2022-10-05 12:09:01 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+function readfileautomatically () {
+        var client = new XMLHttpRequest();
+        client.open('GET', 'states.txt');
+        client.onreadystatechange = function()
+        {
+            if( client.responseText != '' )
+            {
+                var txt = client.responseText.split("\n");
+                let linenum = Math.floor(Math.random() * 2) + 1;
+                let combo = (txt[linenum]);
+                document.getElementById("combo").innerHTML = (combo);
+                alert(combo);
+            }
+        }
+        client.send();
+    }

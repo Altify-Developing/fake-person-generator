5057a21fab09e93b16cbbfafae49af255b8cc17c
Altify <Altify@mail.com>
2022-10-07 10:59:46 -0700

    Update states.js

--- js/states.js
+++ js/states.js
@@ -13,7 +13,7 @@ function start() {
                 let linenum2 = Math.floor(Math.random() * 2999) + 1;
                 let linenum3 = Math.floor(Math.random() * 1961) + 3000;
                 let linenum4 = Math.floor(Math.random() * 4) + 4961;
-                let combo = (randomString(Math.floor(Math.random() * 8) + 3, '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')+txt[linenum]+txt[linenum4]+':'+txt[linenum2]+randomString(Math.floor(Math.random() * 8) + 4, '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')+" | Name: "+txt[linenum]+' '+txt[linenum2]+" | State: "+txt[linenum3]);
+                let combo = (randomString(Math.floor(Math.random() * 8) + 2, '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')+txt[linenum]+txt[linenum4]+':'+txt[linenum2]+randomString(Math.floor(Math.random() * 12) + 1, '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')+" | Name: "+txt[linenum]+' '+txt[linenum2]+" | State: "+txt[linenum3]);
                 document.getElementById("wintotal").innerHTML = (combo);
             }
         }

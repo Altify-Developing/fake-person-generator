a21f22e8650bec6275cfc1773e9471cdae90eeda
Altify <Altify@mail.com>
2022-10-07 11:01:10 -0700

    Update states.js

--- js/states.js
+++ js/states.js
@@ -1,3 +1,12 @@
+function copyElementText(id) {
+    var text = document.getElementById(id).innerText;
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = text;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+}
 function start() {
         setTimeout(r2, 1);
 }

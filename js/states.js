74da880641d4d3502746b8953d68c6c6cce5f4d7
Altify <Altify@mail.com>
2022-10-07 10:51:54 -0700

    Update states.js

--- js/states.js
+++ js/states.js
@@ -1,38 +1,20 @@
 function start() {
-        setTimeout(r1, 1);
-        setTimeout(r2, 10);
+        setTimeout(r2, 1);
 }
-function r1() {
+      function r2() {
         var client = new XMLHttpRequest();
-        client.open('GET', '/resc/states.txt');
+        client.open('GET', '/resc/names.txt');
         client.onreadystatechange = function()
         {
-            if( client.responseText != '' )
+            if( client.responseText != '')
             {
                 let txt = client.responseText.split("\n");
-                let linenum = Math.floor(Math.random() * 1960) + 1;
-                let combo = (txt[linenum]);
-                document.getElementById("wintotal").innerHTML = ("State: "+combo);
+                let linenum = Math.floor(Math.random() * 2999) + 1;
+                let linenum2 = Math.floor(Math.random() * 2999) + 1;
+                let linenum3 = Math.floor(Math.random() * 1961) + 3000;
+                let combo = (randomString(3, '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')+"@gmail.com | Name: "+txt[linenum]+' '+txt[linenum2]+" | State: "+txt[linenum3]);
+                document.getElementById("wintotal").innerHTML = (combo);
             }
         }
         client.send();
-}
-      function r2() {
-        let i = 1;
-        var client2 = new XMLHttpRequest();
-        client2.open('GET', '/resc/names.txt');
-        client2.onreadystatechange = function()
-        {
-            if( client2.responseText != '' && i == 1)
-            {
-                let prev = document.getElementById("wintotal").innerHTML;
-                let txt = client2.responseText.split("\n");
-                let linenum = Math.floor(Math.random() * 2999) + 1;
-                let linenum2 = Math.floor(Math.random() * 2999) + 1;
-                let combo = (txt[linenum]+' '+txt[linenum2]);
-                document.getElementById("wintotal").innerHTML = (prev+" | Name: "+combo);
-                let i = 2;
-            }
-        }
-        client2.send();
     }
